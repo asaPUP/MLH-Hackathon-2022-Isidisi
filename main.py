b91aa83e33d57cbf2e44b94e720fb650d47fcf5f
asaPUP <asael.silvam@gmail.com>
2022-10-15 15:37:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import Toplevel
 import pygame
-from random import randint, choice
+from numpy.random import choice
 from sys import *
 from random import *
 import numpy as np
@@ -10,31 +10,31 @@ class Enemigo(pygame.sprite.Sprite):
         super().__init__()
         
         if type == 'top':
-            enemigo_top_1 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul1.png').convert_alpha())
-            enemigo_top_2 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul2.png').convert_alpha())
-            enemigo_top_3 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul3.png').convert_alpha())
-            enemigo_top_4 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul4.png').convert_alpha())
-            enemigo_top_5 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul5.png').convert_alpha())
-            enemigo_top_6 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul6.png').convert_alpha())
-            enemigo_top_7 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul7.png').convert_alpha())
-            enemigo_top_8 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul8.png').convert_alpha())
+            enemigo_top_1 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnenemigoRojo1.png').convert_alpha())
+            enemigo_top_2 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnenemigoRojo2.png').convert_alpha())
+            enemigo_top_3 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnenemigoRojo3.png').convert_alpha())
+            enemigo_top_4 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnenemigoRojo4.png').convert_alpha())
+            enemigo_top_5 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnenemigoRojo5.png').convert_alpha())
+            enemigo_top_6 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnenemigoRojo6.png').convert_alpha())
+            enemigo_top_7 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnenemigoRojo7.png').convert_alpha())
+            enemigo_top_8 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnenemigoRojo8.png').convert_alpha())
             self.frames = [enemigo_top_1, enemigo_top_2, enemigo_top_3, enemigo_top_4, enemigo_top_5, enemigo_top_6, enemigo_top_7, enemigo_top_8]
-            x_pos = randint(-200,0)
-            y_pos = random.choice([90, 160, 230]) #random entre los tres carriles de arriba
+            x_pos = randint(900,1100)
+            y_pos = choice([90, 160, 230]) #random entre los tres carriles de arriba
             
         elif type == 'bot':
-            enemigo_bot_1 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnemigoRojo1.png').convert_alpha())
-            enemigo_bot_2 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnemigoRojo2.png').convert_alpha())
-            enemigo_bot_3 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnemigoRojo3.png').convert_alpha())
-            enemigo_bot_4 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnemigoRojo4.png').convert_alpha())
-            enemigo_bot_5 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnemigoRojo5.png').convert_alpha())
-            enemigo_bot_6 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnemigoRojo6.png').convert_alpha())
-            enemigo_bot_7 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnemigoRojo7.png').convert_alpha())
-            enemigo_bot_8 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/rojo/EnemigoRojo8.png').convert_alpha())
+            enemigo_bot_1 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul1.png').convert_alpha())
+            enemigo_bot_2 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul2.png').convert_alpha())
+            enemigo_bot_3 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul3.png').convert_alpha())
+            enemigo_bot_4 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul4.png').convert_alpha())
+            enemigo_bot_5 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul5.png').convert_alpha())
+            enemigo_bot_6 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul6.png').convert_alpha())
+            enemigo_bot_7 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul7.png').convert_alpha())
+            enemigo_bot_8 = pygame.transform.scale2x(pygame.image.load('assets/enemigos/azul/EnemigoAzul8.png').convert_alpha())
             
             self.frames = [enemigo_bot_1, enemigo_bot_2, enemigo_bot_3, enemigo_bot_4, enemigo_bot_5, enemigo_bot_6, enemigo_bot_7, enemigo_bot_8]
-            x_pos = randint(900,1100)
-            y_pos = random.choice([340, 410, 480]) #random entre los tres carriles de abajo
+            x_pos = randint(-200,0)
+            y_pos = choice([340, 410, 480]) #random entre los tres carriles de abajo
         
         self.animation_index = 0
         self.image = self.frames[self.animation_index]
@@ -47,12 +47,19 @@ class Enemigo(pygame.sprite.Sprite):
     
     def update(self):
         self.animation_state()
-        self.rect.x -= 6
+        if self.rect.y <= 250:
+            self.rect.x -= 5
+        else:
+            self.rect.x += 5
         self.destroy()
 
     def destroy(self):
-        if self.rect.x <= -100: 
-            self.kill()
+        if self.rect.y <= 250:
+            if self.rect.x >= 1000:
+                self.kill()
+        else:
+            if self.rect.x <= -100:
+                self.kill()
 
 class Jugador(pygame.sprite.Sprite):
     def __init__(self,type):
@@ -90,13 +97,11 @@ class Jugador(pygame.sprite.Sprite):
         self.image = self.jugador_walk[self.jugador_index]
         self.rect = self.image.get_rect(midbottom = (x_pos, y_pos))
 
-    def player_input(self, type):
+    def player_input(self):
         keys = pygame.key.get_pressed()
-        if (keys[pygame.K_UP] and type == 'blue'):
-            self.rect.y -= 70
+        if (keys[pygame.K_UP]):
             print("arriba")
-        elif (keys[pygame.K_DOWN] and type == 'blue'):
-            self.rect.y += 70
+        elif (keys[pygame.K_DOWN]):
             print("abajo")
 
     def animation_state(self):
@@ -128,7 +133,9 @@ red_player = pygame.sprite.GroupSingle()
 red_player.add(Jugador('red'))
 
 obstacle_group = pygame.sprite.Group()
+
 fondo_surface = pygame.image.load("assets/fondov1.png").convert()
+
 textSurface = test_font.render('R0B0 / K1LL', False, 'Black')
 text_rectangle = textSurface.get_rect(center = (450, 250))
 
@@ -136,11 +143,18 @@ enemigo_surface = pygame.image.load('assets/enemigo.png').convert_alpha()
 enemigo_surface = pygame.transform.scale2x(enemigo_surface)
 enemigo_rectangle = enemigo_surface.get_rect(center = (900 - 32, 32))
 
+#Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    obstacle_group.add(Enemigo('top'))
+    obstacle_group.add(Enemigo('bot'))
         
     WIN.blit(fondo_surface,(0,0))
     WIN.blit(textSurface, text_rectangle)
